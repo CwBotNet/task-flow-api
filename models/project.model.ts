2a487a13d7ece51b36516a2ec3b2f0b872fd79fe
Raj sahani <Rajsahaniofficial19@gmail.com>
2026-03-27 23:04:18 +0530

    feat: initialize core models

---
+++ models/project.model.ts
@@ -0,0 +1,42 @@
+import { Document, Schema, Types, model } from "mongoose";
+
+export interface IProject extends Document {
+  userId: Types.ObjectId;
+  name: string;
+  description?: string;
+  status: "pending" | "in-progress" | "completed";
+  priority: "low" | "medium" | "high";
+  dueDate: Date;
+  assignDate: Date;
+  codeLink: string;
+  liveLink: string;
+}
+
+export const ProjectSchema = new Schema<IProject>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    description: { type: String },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+      min: [new Date(), "Due date cannot be in the past"],
+    },
+    assignDate: { type: Date, default: Date.now },
+    codeLink: { type: String, required: true, trim: true },
+    liveLink: { type: String, trim: true },
+  },
+  { timestamps: true },
+);
+
+export default model<IProject>("Project", ProjectSchema);
