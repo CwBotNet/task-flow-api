4d9d0867937705414dd3f58bdbb45cb957897dde
Raj sahani <Rajsahaniofficial19@gmail.com>
2026-04-02 23:39:44 +0530

    refactor(server): wrap startup logic in async startServer function

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { loadEnvFile } from "node:process";
 import { authRouter } from "./routes";
+import { connectDb } from "./db";
 
 loadEnvFile(".env.local");
 
@@ -8,13 +9,25 @@ const PORT = process.env.PORT;
 
 const app: express.Application = express();
 
-app.get("/", (req, res) => {
-  console.log("Hello world..");
-  res.send("Hello World!");
-});
+const startServer = async () => {
+  try {
+    await connectDb();
 
-app.use("/api/auth", authRouter);
+    app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log("server is running on port: ", PORT);
-});
+    app.get("/", (req, res) => {
+      console.log("Hello world..");
+      res.send("Hello World!");
+    });
+
+    app.use("/api/auth", authRouter);
+
+    app.listen(PORT, () => {
+      console.log("server is running on port: ", PORT);
+    });
+  } catch (error) {
+    console.log("Failed to start server :", error);
+  }
+};
+
+startServer();
